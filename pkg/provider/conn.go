33391f2abe060c560f1a1e6acec1ad49b8e5d111
bcmmbaga <bethuelmbaga12@gmail.com>
2024-09-25 17:10:18 +0300

    init provider
    Signed-off-by: bcmmbaga <bethuelmbaga12@gmail.com>

---
+++ pkg/provider/conn.go
@@ -0,0 +1,72 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/daytonaio/daytona/pkg/agent/ssh/config"
+	"github.com/daytonaio/daytona/pkg/docker"
+	"github.com/daytonaio/daytona/pkg/tailscale"
+	"github.com/docker/docker/client"
+	"github.com/google/uuid"
+	"tailscale.com/tsnet"
+)
+
+func (g *GCPProvider) getTsnetConn() (*tsnet.Server, error) {
+	if g.tsnetConn == nil {
+		tsnetConn, err := tailscale.GetConnection(&tailscale.TsnetConnConfig{
+			AuthKey:    *g.NetworkKey,
+			ControlURL: *g.ServerUrl,
+			Dir:        filepath.Join(*g.BasePath, "tsnet", uuid.NewString()),
+			Logf:       func(format string, args ...any) {},
+			Hostname:   fmt.Sprintf("gcp-provider-%s", uuid.NewString()),
+		})
+		if err != nil {
+			return nil, err
+		}
+		g.tsnetConn = tsnetConn
+	}
+
+	return g.tsnetConn, nil
+}
+
+func (g *GCPProvider) waitForDial(workspaceId string, dialTimeout time.Duration) error {
+	tsnetConn, err := a.getTsnetConn()
+	if err != nil {
+		return err
+	}
+
+	dialStartTime := time.Now()
+	for {
+		if time.Since(dialStartTime) > dialTimeout {
+			return fmt.Errorf("timeout: dialing timed out after %f minutes", dialTimeout.Minutes())
+		}
+
+		dialConn, err := tsnetConn.Dial(context.Background(), "tcp", fmt.Sprintf("%s:%d", workspaceId, config.SSH_PORT))
+		if err == nil {
+			dialConn.Close()
+			return nil
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func (g *GCPProvider) getDockerClient(workspaceId string) (docker.IDockerClient, error) {
+	tsnetConn, err := g.getTsnetConn()
+	if err != nil {
+		return nil, err
+	}
+
+	remoteHost := fmt.Sprintf("tcp://%s:2375", workspaceId)
+	cli, err := client.NewClientWithOpts(client.WithDialContext(tsnetConn.Dial), client.WithHost(remoteHost), client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	return docker.NewDockerClient(docker.DockerClientConfig{
+		ApiClient: cli,
+	}), nil
+}
--- pkg/types/targets.go
+++ pkg/types/targets.go
@@ -7,31 +7,10 @@ import (
 )
 
 type TargetOptions struct {
-	RequiredString string  `json:"Required String"`
-	OptionalString *string `json:"Optional String,omitempty"`
-	OptionalInt    *int    `json:"Optional Int,omitempty"`
-	FilePath       *string `json:"File Path"`
 }
 
 func GetTargetManifest() *provider.ProviderTargetManifest {
-	return &provider.ProviderTargetManifest{
-		"Required String": provider.ProviderTargetProperty{
-			Type:         provider.ProviderTargetPropertyTypeString,
-			DefaultValue: "default-required-string",
-		},
-		"Optional String": provider.ProviderTargetProperty{
-			Type:        provider.ProviderTargetPropertyTypeString,
-			InputMasked: true,
-		},
-		"Optional Int": provider.ProviderTargetProperty{
-			Type: provider.ProviderTargetPropertyTypeInt,
-		},
-		"File Path": provider.ProviderTargetProperty{
-			Type:              provider.ProviderTargetPropertyTypeFilePath,
-			DefaultValue:      "~/.ssh",
-			DisabledPredicate: "^default-target$",
-		},
-	}
+	return &provider.ProviderTargetManifest{}
 }
 
 func ParseTargetOptions(optionsJson string) (*TargetOptions, error) {
